7d121e2d29f5e7d9d5d7a925a30e2612db474516
Gaurav Bopardikar <bopardikargaurav@gmail.com>
2024-12-10 23:30:27 +0530

    Modified 'Student Management System' project

--- Student Management System/classes.py
+++ Student Management System/classes.py
@@ -1,4 +1,6 @@
 import json
+from json import JSONDecodeError
+from tabulate import tabulate
 
 class Student:
     def __init__(self, name, grade):
@@ -13,7 +15,7 @@ class Student:
 class StudentManager:
     students = []
     json_file_name = "students_data.json"
-
+    table_style = "grid"
 
     @classmethod
     def _modify_roll_numbers(cls): # Marked as "private" for internal use
@@ -25,13 +27,13 @@ class StudentManager:
         while True:
             roll_number_input = input(prompt)
             if not roll_number_input.isdigit():
-                print("Roll number must be positive integer. Please try again :(")
+                print("Roll number must be positive integer. Please try again.")
                 continue
             elif roll_number_input == '0':
-                print("Roll number cannot be zero. Please try again :(")
+                print("Roll number cannot be zero. Please try again.")
                 continue
             elif int(roll_number_input) not in range(1, len(cls.student_obj_list) + 1):
-                print(f"Student with roll number {roll_number_input} does not exist. Please try again :(")
+                print(f"Student with roll number {roll_number_input} does not exist. Please try again.")
                 continue
 
             return int(roll_number_input)
@@ -40,11 +42,28 @@ class StudentManager:
     def get_valid_input_grade(prompt):
         while True:
             grade_input = input(prompt)
-            if not grade_input.lower() in 'abcdef':
+            if not grade_input or grade_input.lower() not in 'abcdef':
                 print("\nInvalid Grade. Please try again.")
                 continue
 
             return grade_input.upper()
+
+    @staticmethod
+    def get_valid_input_name(prompt):
+        while True:
+            name_input = input(prompt).strip()
+            if len(name_input) > 50 or len(name_input) < 2:
+                print("Name must be between 2 and 50 characters. Please try again.")
+                continue
+            if not name_input[0].isalpha():
+                print("\nInvalid name. Please try again.")
+                continue
+            for char in name_input:
+                if not char.isalpha() and char not in ['-', "'", ' ']:
+                    print("\nInvalid name. Please try again.")
+                    break
+            else:
+                return name_input
 
     @classmethod
     def load_student_data(cls, file):
@@ -53,6 +72,8 @@ class StudentManager:
                 cls.student_obj_list = json.load(student_json_data_file)
         except FileNotFoundError:
             cls.student_obj_list = []
+        except JSONDecodeError:
+            print("JSON data is invalid")
 
     @classmethod
     def _save_student_data(cls, file):
@@ -64,12 +85,13 @@ class StudentManager:
 
     @classmethod
     def add_student(cls):
-        name = input("Enter student's name: ")
+        name_input_prompt = "\nEnter student's name: "
+        name_input = cls.get_valid_input_name(name_input_prompt)
 
         grade_input_prompt = "Enter student's grade (91-100 = A, 81-90 = B, 71-80 = C, 61-70 = D, 51-60 = E, Fail = F): "
         grade_input = cls.get_valid_input_grade(grade_input_prompt)
 
-        new_student = Student(name, grade_input)
+        new_student = Student(name_input, grade_input)
 
         new_student_obj = {
             "Roll No.": len(cls.student_obj_list)+1,
@@ -80,7 +102,7 @@ class StudentManager:
         cls.student_obj_list.append(new_student_obj)
 
         cls._save_student_data(cls.json_file_name)
-        print("Student added successfully!\n")
+        print("Student added successfully!")
 
         cls.view_all_students()
 
@@ -93,30 +115,23 @@ class StudentManager:
         roll_number_input_prompt = "\nEnter student's roll number who you want to remove: "
         roll_number_input = cls.get_valid_input_roll_number(roll_number_input_prompt)
 
-        for student in cls.student_obj_list:
-            if student["Roll No."] == roll_number_input:
-                cls.student_obj_list.remove(student)
+        targeted_student = cls.student_obj_list[roll_number_input-1]
+        targeted_student_name = targeted_student["Name"]
+
+        while True:
+            confirm_remove = input(f"Are you sure you want to remove '{targeted_student_name}'? [Y/N]: ")
+            if confirm_remove.lower() == 'y':
+                cls.student_obj_list.remove(targeted_student)
                 cls._modify_roll_numbers() # Called automatically after removal
                 cls._save_student_data(cls.json_file_name)
-                print(f"\n{student["Name"]} has been removed successfully.")
+                print(f"\n'{targeted_student_name}' has been removed successfully.")
+
+                cls.view_all_students()
                 break
-
-        cls.view_all_students()
-
-    @classmethod
-    def view_student_details(cls):
-        if not cls.student_obj_list:
-            print("\nNo students available to view details. Add students first.")
-            return
-
-        prompt = "\nEnter student's roll number whose details you want to see: "
-        roll_number_input = cls.get_valid_input_roll_number(prompt)
-
-        for student in cls.student_obj_list:
-            if student["Roll No."] == roll_number_input:
-                print("\nHere is the student's details:")
-                print(f"Roll No.: {student["Roll No."]}, Name: {student["Name"]}, Grade: {student["Grade"]}")
+            elif confirm_remove.lower() == 'n':
                 break
+            else:
+                print("Invalid input. Please try again.")
 
     @classmethod
     def update_student_details(cls):
@@ -128,9 +143,10 @@ class StudentManager:
         roll_number_input = cls.get_valid_input_roll_number(roll_number_input_prompt)
 
         while True:
-            update_attribute = input("Enter 1 to update name, 2 to update grade: ")
+            update_attribute = input("\nEnter 1 to update name, 2 to update grade: ")
             if update_attribute == '1':
-                name_input = input("\nEnter updated name of the student: ")
+                name_input_prompt = "\nEnter updated name of the student: "
+                name_input = cls.get_valid_input_name(name_input_prompt)
 
                 for student in cls.student_obj_list:
                     if student["Roll No."] == roll_number_input:
@@ -140,6 +156,9 @@ class StudentManager:
                         else:
                             student["Name"] = name_input
                             cls._save_student_data(cls.json_file_name)
+
+                            print("\nUpdated student details:\n")
+                            cls.display_students_data([cls.student_obj_list[roll_number_input-1]])
                         break
                 break
             elif update_attribute == '2':
@@ -157,22 +176,84 @@ class StudentManager:
                         break
                 break
             else:
-                print("Invalid Input. Try again :)")
+                print("\nInvalid Input. Please try again.")
+
+    @classmethod
+    def display_students_data(cls, students_data):
+        headers = students_data[0].keys()
+        rows = [student.values() for student in students_data]
+
+        print(tabulate(rows, headers, tablefmt=cls.table_style))
+
+    @classmethod
+    def change_table_format(cls):
+        available_styles = {
+            '1': "plain",
+            '2': "grid",
+            '3': "fancy_grid",
+            '4': "pipe",
+            '5': "simple"
+        }
+
+        while True:
+            print(f"\nCurrent table format: {cls.table_style}")
+            print("How would you like the data to be displayed (or press Enter to keep the current)?")
+            print("1. Plain")
+            print("2. Grid")
+            print("3. Fancy Grid")
+            print("4. Pipe")
+            print("5. Simple")
+
+            table_format_choice = input("Enter your choice (1-5): ")
+
+            if not table_format_choice:
+                break
+            if table_format_choice not in available_styles:
+                print("\nInvalid choice. Please try again.")
+                continue
+
+            print(f"Your desired table format '{available_styles[table_format_choice]}' is set.")
+            cls.table_style =  available_styles[table_format_choice]
+
+            break
 
     @classmethod
     def view_all_students(cls):
         if not cls.student_obj_list:
             print("\nNo students available to view. Add students first.")
             return
+        elif len(cls.student_obj_list) == 1:
+            print(f"\nWe have only {len(cls.student_obj_list)} student in our system:\n")
+        else:
+            print(f"\nWe have {len(cls.student_obj_list)} students in our system:\n")
 
-        print("\nThese are all our students:\n")
-        for student in cls.student_obj_list:
-            print(f"{student["Roll No."]}. {student["Name"]}")
+        cls.display_students_data(cls.student_obj_list)
 
-    @staticmethod
-    def display_sorted_data(sorted_data):
-        for student in sorted_data:
-            print(f"Roll No.: {student["Roll No."]}, Name: {student["Name"]}, Grade: {student["Grade"]}")
+        print("Would you like to view details of a specific student?")
+        while True:
+            choice = input("Enter their Roll Number (or press Enter to return to the main menu): ")
+
+            if not choice:
+                break
+            else:
+                if not choice.isdigit():
+                    print("\nRoll number must be positive integer. Please try again.")
+                    continue
+                elif choice == '0':
+                    print("\nRoll number cannot be zero. Please try again.")
+                    continue
+                elif int(choice) not in range(1, len(cls.student_obj_list) + 1):
+                    print(f"\nStudent with roll number {choice} does not exist. Please try again.")
+                    continue
+
+                roll_num = int(choice)
+                targeted_student = cls.student_obj_list[roll_num - 1]
+
+                print("\nHere is the student's details:")
+
+                cls.display_students_data([targeted_student])
+
+                break
 
     @classmethod
     def sort_students(cls):
@@ -186,25 +267,60 @@ class StudentManager:
             print("2. Name")
             print("3. Grade")
             print("4. Go Back to Main Menu")
-            sort_choice = input("Enter your choice: ")
+            sort_choice = input("Enter your choice (1-4): ")
 
             if sort_choice == '1':
                 sorted_by_roll_number_data = sorted(cls.student_obj_list, key=lambda x: x["Roll No."])
                 print("\nStudents sorted by Roll Number:")
-                cls.display_sorted_data(sorted_by_roll_number_data)
+                cls.display_students_data(sorted_by_roll_number_data)
             elif sort_choice == '2':
                 sorted_by_name_data = sorted(cls.student_obj_list, key=lambda x: x["Name"].lower())
                 print("\nStudents sorted by Name:")
-                cls.display_sorted_data(sorted_by_name_data)
+                cls.display_students_data(sorted_by_name_data)
             elif sort_choice == '3':
                 sorted_by_grade_data = sorted(cls.student_obj_list, key=lambda x: x["Grade"])
                 print("\nStudents sorted by Grade:")
-                cls.display_sorted_data(sorted_by_grade_data)
+                cls.display_students_data(sorted_by_grade_data)
             elif sort_choice == '4':
                 print("\nReturning to main menu.")
                 break
             else:
-                print("\nInvalid Choice. Please try again :)")
+                print("\nInvalid Choice. Please try again.")
 
+    @classmethod
+    def view_students_by_grade(cls):
+        if not cls.student_obj_list:
+            print("\nNo students available to view. Add students first.")
+            return
+
+        grade_input_prompt = "\nSpecify the grade you want to filter by (A, B, C, D, E, F): "
+        grade_input = cls.get_valid_input_grade(grade_input_prompt)
+
+        target_data = [student for student in cls.student_obj_list if student["Grade"] == grade_input]
+
+        if not target_data:
+            print(f"No students found with grade {grade_input}.")
+            return
+
+        print(f"\nAll {len(target_data)} students with grade {grade_input}:\n")
+
+        cls.display_students_data(target_data)
+
+    @classmethod
+    def manage_data(cls):
+        while True:
+            print("\nData Management Options:")
+            print("1. Import Students from JSON File")
+            print("2. Export Students to JSON File")
+            print("3. Back to Main Menu")
+
+            choice = input("\nEnter your choice (1-3): ")
+
+            if choice not in '123':
+                print("Invalid choice. Please try again.")
+                continue
+
+            if choice == '2':
+                pass
 
 
--- Student Management System/main.py
+++ Student Management System/main.py
@@ -2,32 +2,38 @@ from classes import StudentManager
 
 
 def main():
-    print("Welcome to Student Management System:")
+    print("Welcome to the Student Management System!")
     StudentManager.load_student_data(StudentManager.json_file_name)
     while True:
-        print("\n1. Add Student")
-        print("2. View Student Details")
-        print("3. Update Student Grade")
-        print("4. View All Students")
-        print("5. Remove Student")
-        print("6. Sort Students")
-        print("7. Exit")
+        print("\n1. View All Students")
+        print("2. Sort Students")
+        print("3. View Students by Grade")
+        print("4. Change Table Format")
+        print("5. Add Student")
+        print("6. Update Student Details")
+        print("7. Remove Student")
+        print("8. Data Management")
+        print("9. Exit\n")
 
-        choice = input("Enter your choice: ")
+        choice = input("Enter your choice (1-9): ")
 
         if choice == '1':
-            StudentManager.add_student()
-        elif choice == '2':
-            StudentManager.view_student_details()
-        elif choice == '3':
-            StudentManager.update_student_details()
-        elif choice == '4':
             StudentManager.view_all_students()
-        elif choice == '5':
-            StudentManager.remove_student()
-        elif choice == '6':
+        elif choice == '2':
             StudentManager.sort_students()
+        elif choice == '3':
+            StudentManager.view_students_by_grade()
+        elif choice == '4':
+            StudentManager.change_table_format()
+        elif choice == '5':
+            StudentManager.add_student()
+        elif choice == '6':
+            StudentManager.update_student_details()
         elif choice == '7':
+            StudentManager.remove_student()
+        elif choice == '8':
+            StudentManager.manage_data()
+        elif choice == '9':
             print("\nThank You For Visiting :)")
             break
         else:
